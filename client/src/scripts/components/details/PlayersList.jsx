3233fb3820bb6571ffebfbe77dce305d6002b2b0
James George <james.george@nuance.com>
2015-08-27 18:55:38 -0400

    scroll to selected player

--- client/src/scripts/components/details/PlayersList.jsx
+++ client/src/scripts/components/details/PlayersList.jsx
@@ -52,6 +52,13 @@ var PlayersList = React.createClass({
         this.setState({
             player: PlayerStore.getCurrent()
         });
+        if(PlayerStore.getCurrent().get('id')) {
+            var table = $(this.getDOMNode()).find('.table-body');
+            var scroll = table.scrollTop();
+            var player = $(this.getDOMNode()).find('#player_' + PlayerStore.getCurrent().get('id'));
+            var playerTop = player.offset().top;
+            table.scrollTop(scroll + playerTop - table.offset().top - player.height());
+        }
     },
 
     onPlayersChange() {
